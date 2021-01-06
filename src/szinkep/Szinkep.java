ff8c9c37b5c657d0ad5c8f9dbef52da07515538f
GergelyVajda <51296438+GergelyVajda@users.noreply.github.com>
2021-01-06 23:25:14 +0100

    kész, de 7-es nem jó

--- src/szinkep/Szinkep.java
+++ src/szinkep/Szinkep.java
@@ -8,7 +8,9 @@ package szinkep;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -169,21 +171,87 @@ public class Szinkep {
                 }
 
             }
+            //teteje
             for (int i = 0; i < 150; i++) {
                 pixelek[i][0] = 0;
                 pixelek[i][1] = 0;
                 pixelek[i][2] = 0;
             }
-            for (int i = 2500; i < 2350; i--) {
-                pixelek[i][0] = 0;
-                pixelek[i][1] = 0;
-                pixelek[i][2] = 0;
-            }
+            //alja
             for (int i = 0; i < 2500; i++) {
+                if (i > 2350) {
+                    pixelek[i][0] = 0;
+                    pixelek[i][1] = 0;
+                    pixelek[i][2] = 0;
+                }
+            }
+            //ellenőrző kiírató
+            /*for (int i = 0; i < 2500; i++) {
                 System.out.println(pixelek[i][0]);
                 System.out.println(pixelek[i][1]);
                 System.out.println(pixelek[i][2]);
+            }*/
+
+            //6
+            FileWriter fw = new FileWriter("keretes.txt");
+            PrintWriter pw = new PrintWriter(fw);
+
+            for (int i = 0; i < 2500; i++) {
+                pw.println(pixelek[i][0] + " " + pixelek[i][1] + " " + pixelek[i][2]);
             }
+            pw.close();
+
+            //7
+            System.out.println("7. feladat");
+            //pixel koordináták generálása
+            Integer[][] sarga = new Integer[2500][3];
+            int hor = 0;
+            int ver = 1;
+            for (int i = 0; i < 2500; i++) {
+                hor++;
+                sarga[i][0] = hor;
+                sarga[i][1] = ver;
+                sarga[i][2] = 0;
+                if (hor == 50) {
+                    hor = 0;
+                    ver++;
+                }
+            }
+            //ellenőrzés
+            /*for (int i = 0; i < 2500; i++) {
+                System.out.println(sarga[i][0]+" "+sarga[i][1]+" "+sarga[i][2]);
+            }*/
+
+            //sárga keresés és beírás a koordináták mellé = megvan a téglalap
+            int osszSarga = 0;
+            for (int i = 0; i < 2500; i++) {
+                if (pixelek[i][0] == 255
+                        && pixelek[i][1] == 255
+                        && pixelek[i][2] == 0) {
+                    sarga[i][2] = 1;
+                    osszSarga++;
+                }
+            }
+            //eredmény leolvasása
+            int kezdH = 0;
+            int kezdV = 0;
+            int vegH = 0;
+            int vegV = 0;
+            for (int i = 0; i < 2500; i++) {
+                if (sarga[i][2] == 1) {
+                    kezdH = sarga[i][0];
+                    kezdV = sarga[i][1];
+                }
+            }
+            for (int i = 2499; i >= 0; i--) {
+                 if (sarga[i][2] == 1) {
+                    vegH = sarga[i][0];
+                    vegV = sarga[i][1];
+                }
+            }
+            System.out.println("Kezd: "+kezdH+", "+kezdV);
+            System.out.println("Vége: "+vegH+", "+vegV);
+            System.out.println("Képpontok száma: "+osszSarga+"db");
 
         } catch (FileNotFoundException ex) {
             System.out.println("Nem található a file!");
